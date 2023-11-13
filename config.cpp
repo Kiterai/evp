a51d4c6f85e5296bd1a0ba7b6353b005e2068167
Kiterai <kiterai@foolslab.net>
2023-11-13 16:28:54 +0900

    メインターゲットがexecutableでないときにエラー

--- config.cpp
+++ config.cpp
@@ -3,24 +3,31 @@
 Config::Config() {
     auto config = YAML::LoadFile(config_file_name);
 
-    if (const auto c_main_target = config["main_target"]; c_main_target.IsDefined())
-        main_target = c_main_target.as<std::string>();
     const auto c_targets = config["targets"];
     if (!c_targets.IsMap()) {
         throw std::runtime_error("'targets' must be map");
     }
+
     for (const auto &c_target : c_targets) {
         targets.push_back({});
         Target &target = targets.back();
 
         target.name = c_target.first.as<std::string>();
-        if (target.type == Target::TargetType::Executable && main_target.empty())
-            main_target = target.name;
+
+        if (const auto c_main_target = config["mainTarget"]; c_main_target.IsDefined() && c_main_target.IsScalar()) {
+            const auto tmp = c_main_target.as<std::string>();
+            if (const auto type_node = c_targets[tmp]["type"]; type_node.IsDefined() && c_targets[tmp]["type"].as<Target::TargetType>() != Target::TargetType::Executable)
+                throw std::runtime_error("main target not executable");
+            main_target = tmp;
+        }
 
         if (auto c_type = c_target.second["type"]; c_type.IsDefined())
             target.type = c_type.as<Target::TargetType>();
         else
             target.type = Target::TargetType::Executable;
+
+        if (target.type == Target::TargetType::Executable && main_target.empty())
+            main_target = target.name;
 
         target.src = c_target.second["src"].as<std::vector<std::string>>();
 
